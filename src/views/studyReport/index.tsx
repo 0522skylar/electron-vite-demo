62029601bfef44b834b3636f9a9df56738a0b7c3
huanghaiyan <2031499619@qq.com>
2022-06-16 18:59:02 +0800

    feat: 新增学习报告的左边

--- src/views/studyReport/index.tsx
+++ src/views/studyReport/index.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import BetterScroll from 'better-scroll'
 import butterfly from '@/assets/image/icon_butterfly@2x.be6101e3.png'
 import flower from '@/assets/image/icon_deco_flower@2x.a3712ddb.png'
-import { snow } from './constant'
+import { finishIcon, snow } from './constant'
 
 
 export default function StudyReport() {
@@ -12,7 +12,7 @@ export default function StudyReport() {
 
   useEffect(() => {
     const warp = document.querySelector("#content");
-    new BetterScroll( warp , {
+    new BetterScroll(warp, {
       scrollbar: {
         fade: false,
       },
@@ -23,40 +23,56 @@ export default function StudyReport() {
       <div className={style.bg}>
         <div className={style.main}>
           <div className={style.leftBox}>
-            <img src={snow} alt="" />
+            <div className={style.work_box}>
+              <div className={style.pic_frame_box}>
+                <img src="https://ai-cdn.oss-cn-shenzhen.aliyuncs.com/81fa86b1c2e1ed72f2f84ec4b9344752.jpg" className={style.img} />
+              </div>
+            </div>
+            <img src={snow} className={style.icon_star_left}/>
+            <img src={snow} className={style.icon_star_right}/>
           </div>
           <div className={style.rightBox}>
             <div className={style.scroll_wrapper} id="content">
               <div className={style.scroll_box}>
-              <ul className={style.landscape}>
-                <li className={style.study_data_item}>
-                  <div className={style.time}>0分钟</div>
-                  <span className={style.desc}>学习时长</span>
-                </li>
-                <li className={style.study_data_item}>
-                  <div className={style.time}>0天</div>
-                  <span className={style.desc}>累计学习</span>
-                </li>
-                <li className={style.study_data_item}>
-                  <div className={style.time}>0个</div>
-                  <span className={style.desc}>累计作品</span>
-                </li>
-              </ul>
-              <div className={style.study_content} ref={landscape} >
+                <ul className={style.landscape}>
+                  <li className={style.study_data_item}>
+                    <div className={style.time}>0分钟</div>
+                    <span className={style.desc}>学习时长</span>
+                  </li>
+                  <li className={style.study_data_item}>
+                    <div className={style.time}>0天</div>
+                    <span className={style.desc}>累计学习</span>
+                  </li>
+                  <li className={style.study_data_item}>
+                    <div className={style.time}>0个</div>
+                    <span className={style.desc}>累计作品</span>
+                  </li>
+                </ul>
+                <div className={style.study_content} ref={landscape} >
                   <div className={style.title}>内容名称</div>
                   <div className={style.content}>
-                  内容Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore natus error vitae officia odit illo corporis officiis in, maxime a sunt mollitia perferendis, beatae fugit ab necessitatibus similique aperiam. Ad?
-                  内容Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore natus error vitae officia odit illo corporis officiis in, maxime a sunt mollitia perferendis, beatae fugit ab necessitatibus similique aperiam. Ad?
-                  内容Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore natus error vitae officia odit illo corporis officiis in, maxime a sunt mollitia perferendis, beatae fugit ab necessitatibus similique aperiam. Ad?
-                  内容Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore natus error vitae officia odit illo corporis officiis in, maxime a sunt mollitia perferendis, beatae fugit ab necessitatibus similique aperiam. Ad?
-                  内容Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore natus error vitae officia odit illo corporis officiis in, maxime a sunt mollitia perferendis, beatae fugit ab necessitatibus similique aperiam. Ad?
-                  内容Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore natus error vitae officia odit illo corporis officiis in, maxime a sunt mollitia perferendis, beatae fugit ab necessitatibus similique aperiam. Ad?
+                    内容Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore natus error vitae officia odit illo corporis officiis in, maxime a sunt mollitia perferendis, beatae fugit ab necessitatibus similique aperiam. Ad?
                   </div>
+                  <div className={style.title}>掌握知识点</div>
+                  <ul className={style.knowledge_list}>
+                    <li className={style.knowledge_list_item}>
+                      <img src={finishIcon} className={style.icon_tick} />
+                      <p className={style.knowledge_desc}>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </li>
+                    <li className={style.knowledge_list_item}>
+                      <img src={finishIcon} className={style.icon_tick}/>
+                      <p className={style.knowledge_desc}>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </li>
+                    <li className={style.knowledge_list_item}>
+                      <img src={finishIcon} className={style.icon_tick}/>
+                      <p className={style.knowledge_desc}>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </li>
+                  </ul>
                 </div>
               </div>
             </div>
-            <img src={butterfly} alt="" className={style.butterfly} />
-            <img src={flower} alt="" className={style.flower} />
+            <img src={butterfly} className={style.butterfly} />
+            <img src={flower} className={style.flower} />
           </div>
         </div>
       </div>
