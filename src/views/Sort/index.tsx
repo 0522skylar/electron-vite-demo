f7ca6fb7d8b6f417bbcebc5197e9881b0732a009
huanghaiyan <2031499619@qq.com>
2022-07-06 23:43:51 +0800

    feat: 单词列表显示

--- src/views/Sort/index.tsx
+++ src/views/Sort/index.tsx
@@ -4,6 +4,7 @@ import { Button } from 'antd';
 import style from './index.module.scss'
 // import data from '../../data/gaokao/list.json'
 export default function Sort() {
+  const [wordList, setWordList]= useState<Array<Array<string>>>([]);
   useEffect(() => {
     // const data  = require('../../data/gaokao/list.json');
     // console.log(data);
@@ -11,50 +12,57 @@ export default function Sort() {
       method: 'get',
     }).then((response) => response.json())
     .then((res) => {
-      console.log(res, '-----------')
+      handlerWordList(res.data.list)
     }).catch((err) => {
       console.log(err, 'error');
     })
   }, [])
 
+  const handlerWordList = (list: Array<string>) => {
+    let ans:Array<Array<string>> = [];
+
+    list.forEach((item: string) => {
+      let firstWord: string = item.slice(0, 1).toUpperCase();
+      if(ans[firstWord.charCodeAt(0) - 65]) {
+        ans[firstWord.charCodeAt(0) - 65].push(item);
+      }
+      else {
+        ans[firstWord.charCodeAt(0) - 65] = [item];
+      }
+    })
+    setWordList(ans);
+  }
+
   return (
     <>
       <div className={style.contianer}>
         <h1>单词本</h1>
+
+        {
+          wordList.map((item: Array<string>, index) => {
+            return (
+              <div className={style.sides} key={index}>
+                <p id={'tt' + String.fromCharCode(index + 65)}>{String.fromCharCode(index + 65)}</p>
+                {item.map((nItem) => <div className={style.line} key={nItem}>{nItem}</div> )}
+              </div>
+            )
+          })
+        }
+
         <div className={style.rightBox}>
-          <div className={style.word}>A</div>
-          <div className={style.word}>A</div>
-          <div className={style.word}>A</div>
-          <div className={style.word}>A</div>
-          <div className={style.word}>A</div>
-          <div className={style.word}>A</div>
-          <div className={style.word}>A</div>
-        </div>
-        <div className={style.sides}>
-          <h3>A</h3>
-          <div className={style.line}>aside</div>
-          <div className={style.line}>aside</div>
-          <div className={style.line}>aside</div>
-          <div className={style.line}>aside</div>
-          <div className={style.line}>aside</div>
-        </div>
-
-        <div className={style.sides}>
-          <h3>A</h3>
-          <div className={style.line}>aside</div>
-          <div className={style.line}>aside</div>
-          <div className={style.line}>aside</div>
-          <div className={style.line}>aside</div>
-          <div className={style.line}>aside</div>
-        </div>
-
-        <div className={style.sides}>
-          <h3>A</h3>
-          <div className={style.line}>aside</div>
-          <div className={style.line}>aside</div>
-          <div className={style.line}>aside</div>
-          <div className={style.line}>aside</div>
-          <div className={style.line}>aside</div>
+          <div className={style.lis}>
+            <ul>
+            {
+              wordList.map((item: Array<string>, index) => {
+                return <li className={style.word} key={index}>
+                  <a href={'#tt' + String.fromCharCode(index + 65)}>
+                    {String.fromCharCode(index + 65)}
+                  </a>
+                </li>
+              })
+            }
+            </ul>
+          </div>
         </div>
       </div>
     </>
