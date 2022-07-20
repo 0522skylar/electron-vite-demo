db182ce0cfb0c8c3ee4916e86161d2523220190b
huanghaiyan <2031499619@qq.com>
2022-07-21 00:40:36 +0800

    feat: 新增my页面内容替换

--- src/views/My/index.tsx
+++ src/views/My/index.tsx
@@ -1,6 +1,5 @@
-import type { RadioChangeEvent } from 'antd';
 import { Radio, Tabs } from 'antd';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import style from './index.module.scss'
 import headImg from '@/assets/image/myHeadImg.jpg';
 
@@ -14,9 +13,19 @@ interface StoryItem {
 }
 export default function My() {
 
+  const [content, setContent] = useState<Array<StoryItem>>([]); // 展示的内容
   const [fable, setFable] = useState<Array<StoryItem>>([]); // 寓言
   const [beforeBed, setBeforeBed] = useState<Array<StoryItem>>([]); // 睡前
   const [fairy, setFairy] = useState<Array<StoryItem>>([]); // 童话
+
+  const [activeSub, setActiveSub] = useState<Number>(0)
+  const [subList, setSubList] = useState<Array<String>>([
+    '寓言', '睡前','童话','益智', '口才','魔力','围棋'
+  ])
+
+  const [isTop, setIsTop] = useState(true);
+
+  const watchDOM = useRef(null)
   useEffect(() => {
     fetch('http://localhost:80/admin/story', {
       method: 'get',
@@ -27,6 +36,16 @@ export default function My() {
     }).catch((err) => {
       console.log(err, 'error');
     })
+    const dom: HTMLElement = watchDOM.current  as unknown as HTMLElement;
+    let isVisibility = true;
+    const io = new IntersectionObserver(() => {
+      isVisibility = !isVisibility;
+      setIsTop(isVisibility)
+    })
+    io.observe(dom);
+    return () => {
+      io.disconnect();
+    }
   }, [])
 
   const handlerStory = (list: Array<StoryItem>) => {
@@ -51,6 +70,20 @@ export default function My() {
     setFable(yan)
     setBeforeBed(bed)
     setFairy(tong)
+    setContent(yan)
+  }
+
+  const selectSubTab = (index: number) => {
+    if(index === 0) {
+      setContent(fable)
+    }
+    else if(index === 1) {
+      setContent(beforeBed)
+    }
+    else {
+      setContent(fairy)
+    }
+    setActiveSub(index);
   }
   return (
     <>
@@ -61,30 +94,34 @@ export default function My() {
               <img src={headImg} />
             </div>
             <div className={style.userName}>天天</div>
-            <div className={style.userDesc}>he more you like yourself, the less you are like anyone else, which makes you unique. </div>
+            <div className={style.userDesc} ref={watchDOM}>he more you like yourself, the less you are like anyone else, which makes you unique. </div>
           </div>
         </div>
 
-        <div className={style.twoMainTab}>
+        <div className={[style.twoMainTab, isTop ? null : style.isTop].join(' ')}>
           <div className={style.routerTab}>
             <div className={style.childRoute}>字谜</div>
             <div className={`${style.childRoute} ${style.activeRoute}`}>故事</div>
             <div className={style.childRoute}>英语</div>
           </div>
           <div className={style.subjectTab}>
-            <div className={style.sub_active}>寓言</div>
-            <div>睡前</div>
-            <div>童话</div>
-            <div>魔力</div>
-            <div>益智</div>
-            <div>口才</div>
-            <div>围棋</div>
+            {
+              subList.map((item, index) => {
+                return (
+                  <div className={activeSub === index ? style.sub_active : ''}
+                  key={index}
+                  onClick={() => selectSubTab(index)}
+                  >{ item }</div>
+                )
+              })
+            }
+
           </div>
         </div>
 
         <div className={style.content}>
           {
-            fable.map((item) => (
+            content.map((item) => (
               <div className={style.box} key={item._id}>
                 <div className={style.title}>{ item.title }</div>
                 <div className={style.type}>{ item.storytype }</div>
