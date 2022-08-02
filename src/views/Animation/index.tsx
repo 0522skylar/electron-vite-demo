cae1973e533e10b2ccddda751af44c8218299b41
huanghaiyan <2031499619@qq.com>
2022-08-02 09:43:56 +0800

    feat: 完成动画页面

--- src/views/Animation/index.tsx
+++ src/views/Animation/index.tsx
@@ -1,69 +1,19 @@
-import React, { useLayoutEffect, useState, useRef } from 'react';
-import styles from './index.module.scss'
+import React from "react";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { DndProvider } from "react-dnd";
+import Demo1 from "./demo1";
+import Demo2 from "./demo2";
+import Demo3 from "./demo3";
+import styles from "./index.module.scss";
 
-//https://github.com/MinJieLiu/react-flip-demo/blob/main/src/components/Flipper/FlipContext.ts
-
-//https://minjieliu.github.io/react-flip-demo/
-export default function ListShuffler() {
-  const [data, setData] = useState([0, 1, 2, 3, 4, 5]);
-
-  const listRef = useRef<HTMLDivElement>(null);
-
-  function createChildElementRectMap(nodes: HTMLElement | null | undefined) {
-    if (!nodes) {
-      return new Map();
-    }
-    const elements = Array.from(nodes.childNodes) as HTMLElement[];
-    // 使用节点作为 Map 的 key 存储当前快照，下次直接用 node 引用取值，相当方便
-    return new Map(elements.map((node) => [node, node.getBoundingClientRect()]));
-  }
-  // 使用 ref 存储 DOM 之前的位置信息
-  const lastRectRef = useRef<Map<HTMLElement, DOMRect>>(new Map());
-  
-  function handleAdd() {
-    // 添加一条到顶部，让后面节点运动
-    setData((prev) => [prev.length, ...prev]);
-    // 并存储改变前的 DOM 快照
-    lastRectRef.current = createChildElementRectMap(listRef.current);
-  }
-
-  useLayoutEffect(() => {
-    // 改变后的 DOM 快照，此时 UI 并未更新
-    const currentRectMap = createChildElementRectMap(listRef.current);
-    // 遍历之前的快照
-    lastRectRef.current.forEach((prevRect, node) => {
-      // 前后快照的 DOM 引用一样，可以直接获取
-      const currentRect = currentRectMap.get(node);
-
-      // Invert
-      const invert = {
-        left: prevRect.left - currentRect.left,
-        top: prevRect.top - currentRect.top,
-      };
-
-      const keyframes = [
-        {
-          transform: `translate(${invert.left}px, ${invert.top}px)`,
-        },
-        { transform: 'translate(0, 0)' },
-      ];
-
-      // Play 执行动画
-      node.animate(keyframes, {
-        duration: 800,
-        easing: 'cubic-bezier(0.25, 0.8, 0.25, 1)',
-      });
-    });
-  }, [data]);
-
+export default function App() {
   return (
-    <div className={styles.list} ref={listRef}>
-      {data.map((item) => (
-        <div key={item} className={styles.item}>
-          {item}
-        </div>
-      ))}
-    </div>
+    <DndProvider backend={HTML5Backend}>
+      <div className={styles.app}>
+        <Demo1 />
+        <Demo2 />
+        <Demo3 />
+      </div>
+    </DndProvider>
   );
 }
-
